c45573ab070cdee2d5e1b18765b673c5f1fa9fb3
drygdryg <drygdryg2014@yandex.com>
2020-09-25 15:08:09 +0300

    Fix typos

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         with open(SESSION_FILE, 'rb') as f:
             http = pickle.load(f)
     except FileNotFoundError:
-        print('Please provide Weblance.net credentials')
+        print('Please provide Weblancer.net credentials')
         while 1:
             login = input('Login: ')
             password = input('Password: ')
